04dd61ababd1b224b0960fd4b60c06dc37bb29f4
jefferson <jeff.keiryw@gmail.com>
2022-03-17 19:09:45 -0300

    atualizar marca

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -61,7 +61,8 @@ class MarcaController extends Controller
      */
     public function update(Request $request, Marca $marca)
     {
-        //
+        $marca->update($request->all());
+        return $marca;
     }
 
     /**
